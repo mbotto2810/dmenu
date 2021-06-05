9dd856819e162701c02110a893eb737a5a37bab8
Maruan <maruanbakriottoni@gmail.com>
2021-06-04 21:08:47 -0300

    auto

--- config.h
+++ config.h
@@ -4,7 +4,7 @@
 static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"sans:size=8",
+	"ComicMono:size=8",
 	"JoyPixels:pixelsize=6:antialias=true:autohint=true"
 };
 static const unsigned int bgalpha = 0xe0;
